f31f70494c85bd978337ef33aa818c97cca13928
Ironipad64 <Ironipad64@gmail.com>
2021-02-17 13:44:45 -0800

    added redux stuff

--- app/src/index.js
+++ app/src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import { Provider } from 'react-redux';
+
+// import { reducer } from './reducers';
+
+// const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
     <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
